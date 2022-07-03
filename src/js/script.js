180092007412319a35709c416d75b846ae2a731b
Sergey <graygmai1@gmail.com>
2022-07-03 22:57:53 +0500

    upd dist

--- src/js/script.js
+++ src/js/script.js
@@ -4,154 +4,178 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     //slider
-    const sliderInner = document.querySelector('.m-card__slider');
-    const sliderOne = document.querySelector('.m-card__item');
-    const sliderWidth = sliderOne.offsetWidth;
-    const styleGap = parseInt(getComputedStyle(sliderInner).gap);
-    const sliderAll = Array.from(document.querySelectorAll('.m-card__item'));
-    const width = -styleGap + (sliderWidth + styleGap) * sliderAll.length
-    const COUNT_SLIDER_ON_SITE = 4;
 
-    sliderInner.style.width = width + "px";
-    sliderInner.style.display = "flex";
+    (function() {
 
 
-    let scrollToPx = 0;
-    sliderInner.addEventListener('wheel', (e) => {
-        e.preventDefault();
-        let buffScroll = 0;
+        const sliderInner = document.querySelector('.m-card__slider');
+        const sliderOne = document.querySelector('.m-card__item');
+        const sliderWidth = sliderOne.offsetWidth;
+        const styleGap = parseInt(getComputedStyle(sliderInner).gap);
+        const sliderAll = Array.from(document.querySelectorAll('.m-card__item'));
+        const width = -styleGap + (sliderWidth + styleGap) * sliderAll.length
+        const COUNT_SLIDER_ON_SITE = 4;
 
-        if (e.deltaY > 0) {
-            buffScroll += (sliderWidth + styleGap);
-        } else {
-            buffScroll -= (sliderWidth + styleGap);
-        }
-
-
-        if (scrollToPx + buffScroll > 0) {
-
-        } else if (scrollToPx + buffScroll < -((sliderAll.length - COUNT_SLIDER_ON_SITE) * (sliderWidth + styleGap))) {
-
-        } else {
-            scrollToPx += buffScroll;
-        }
+        sliderInner.style.width = width + "px";
+        sliderInner.style.display = "flex";
 
 
+        let scrollToPx = 0;
+        sliderInner.addEventListener('wheel', (e) => {
+            e.preventDefault();
+            let buffScroll = 0;
+
+            if (e.deltaY > 0) {
+                buffScroll += (sliderWidth + styleGap);
+            } else {
+                buffScroll -= (sliderWidth + styleGap);
+            }
+
+
+            if (scrollToPx + buffScroll > 0) {
+
+            } else if (scrollToPx + buffScroll < -((sliderAll.length - COUNT_SLIDER_ON_SITE) * (sliderWidth + styleGap))) {
+
+            } else {
+                scrollToPx += buffScroll;
+            }
 
 
 
-        sliderInner.style.transform = `translateX(${scrollToPx}px)`;
 
 
-    })
+            sliderInner.style.transform = `translateX(${scrollToPx}px)`;
+
+
+        })
+
+
+
+    })();
+
+
+
 
 
 
     //accordion
 
-
-    const btn = document.querySelectorAll('[data-acc="btn_close"]');
-
-    console.log(btn);
-
-    btn.forEach(btn => {
-
-        const wrap = btn.closest('[data-acc="wrap"]');
-        const content = wrap.querySelector('[data-acc="content"]');
-        const wrapHeight = content.scrollHeight;
-
-        content.style.maxHeight = 0;
-
-        console.log(wrapHeight)
-
-        btn.addEventListener('click', function(e) {
-
-            const btn = e.target;
-            const btnAtr = btn.getAttribute('data-acc')
-            const btnName = btn.getAttribute('data-acc-name')
+    (function() {
 
 
 
-            if (btnAtr == "btn_close") {
-                btn.setAttribute('data-acc', 'btn_open')
-                content.style.maxHeight = wrapHeight + "px";
 
-                switch (btnName) {
-                    case "plane":
-                        btn.innerText = "Скрыть учебный план"
-                        btn.classList.toggle('active');
-                        break;
-                    case "step":
-                        btn.innerText = "Скрыть описание"
-                        break;
+        const btn = document.querySelectorAll('[data-acc="btn_close"]');
 
-                    default:
-                        break;
+
+        btn.forEach(btn => {
+
+            const wrap = btn.closest('[data-acc="wrap"]');
+            const content = wrap.querySelector('[data-acc="content"]');
+            const wrapHeight = content.scrollHeight;
+
+            content.style.maxHeight = 0;
+
+
+            btn.addEventListener('click', function(e) {
+
+                const btn = e.target;
+                const btnAtr = btn.getAttribute('data-acc')
+                const btnName = btn.getAttribute('data-acc-name')
+
+
+
+                if (btnAtr == "btn_close") {
+                    btn.setAttribute('data-acc', 'btn_open')
+                    content.style.maxHeight = wrapHeight + "px";
+
+                    switch (btnName) {
+                        case "plane":
+                            btn.innerText = "Скрыть учебный план"
+                            btn.classList.toggle('active');
+                            break;
+                        case "step":
+                            btn.innerText = "Скрыть описание"
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                } else {
+                    btn.setAttribute('data-acc', 'btn_close')
+                    content.style.maxHeight = 0;
+                    switch (btnName) {
+                        case "plane":
+                            btn.innerText = "Показать учебный план"
+                            btn.classList.toggle('active');
+
+                            const subBtn = document.querySelectorAll('[data-acc-name="step"]')
+                            subBtn.forEach(item => {
+                                const atr = item.getAttribute('data-acc')
+                                if (atr == 'btn_open') {
+                                    item.click();
+                                }
+                            })
+
+                            break;
+                        case "step":
+                            btn.innerText = "Описание ступени"
+                            break;
+
+                        default:
+                            break;
+                    }
                 }
 
-            } else {
-                btn.setAttribute('data-acc', 'btn_close')
-                content.style.maxHeight = 0;
-                switch (btnName) {
-                    case "plane":
-                        btn.innerText = "Показать учебный план"
-                        btn.classList.toggle('active');
-
-                        const subBtn = document.querySelectorAll('[data-acc-name="step"]')
-                        subBtn.forEach(item => {
-                            const atr = item.getAttribute('data-acc')
-                            if (atr == 'btn_open') {
-                                item.click();
-                            }
-                        })
-
-                        break;
-                    case "step":
-                        btn.innerText = "Описание ступени"
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-
-            console.log(btnAtr)
 
 
 
 
-        })
+            })
 
 
 
-    });
+        });
+
+
+
+    })();
+
+
+
+
 
 
     //accordion - Recomend
-    const btnRecomend = document.querySelectorAll('.recomend__btn');
 
-    btnRecomend.forEach(btn => {
-        btn.addEventListener('click', () => {
-            const content = btn.nextElementSibling
-            const maxHeight = parseInt(getComputedStyle(content).maxHeight)
+    (function() {
+        const btnRecomend = document.querySelectorAll('.recomend__btn');
 
-            if (maxHeight == 0) {
-                btnRecomend.forEach(btn => {
-                    if (btn.classList.contains('active')) {
-                        btn.classList.toggle('active');
-                    }
-                    btn.nextElementSibling.style.maxHeight = 0;
-                })
+        btnRecomend.forEach(btn => {
+            btn.addEventListener('click', () => {
+                const content = btn.nextElementSibling
+                const maxHeight = parseInt(getComputedStyle(content).maxHeight)
 
-                content.style.maxHeight = content.scrollHeight + 'px';
-                btn.classList.toggle('active')
-            } else {
-                content.style.maxHeight = 0;
-                btn.classList.toggle('active')
-            }
+                if (maxHeight == 0) {
+                    btnRecomend.forEach(btn => {
+                        if (btn.classList.contains('active')) {
+                            btn.classList.toggle('active');
+                        }
+                        btn.nextElementSibling.style.maxHeight = 0;
+                    })
 
+                    content.style.maxHeight = content.scrollHeight + 'px';
+                    btn.classList.toggle('active')
+                } else {
+                    content.style.maxHeight = 0;
+                    btn.classList.toggle('active')
+                }
+
+            })
         })
-    })
+
+    })();
 
 
 
@@ -164,7 +188,6 @@ document.addEventListener('DOMContentLoaded', () => {
             const template = masked,
                 def = template.replace(/\D/g, ""),
                 val = this.value.replace(/\D/g, "");
-            // console.log(template);
             let i = 0,
                 newValue = template.replace(/[_\d]/g, function(a) {
                     return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
@@ -198,149 +221,122 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-    //modals
+    //modals && Form
 
-    const modalBtn = document.querySelectorAll("[data-modal-btn]")
-    const overlay = document.querySelector(".modal__overlay")
-    const modalWindowForm = document.querySelector("#modal-form")
-    const modalWindowALL = document.querySelectorAll(".modal__window")
-    const modalClose = document.querySelectorAll(".modal__close")
+    (function() {
 
-    modalClose.forEach(item => {
-        item.addEventListener('click', modalOff);
-    });
+        //modals
 
-    modalBtn.forEach(btn => {
+        const modalBtn = document.querySelectorAll("[data-modal-btn]")
+        const overlay = document.querySelector(".modal__overlay")
+        const modalWindowForm = document.querySelector("#modal-form")
+        const modalWindowALL = document.querySelectorAll(".modal__window")
+        const modalClose = document.querySelectorAll(".modal__close")
 
-        btn.addEventListener("click", () => {
-            modalOn(modalWindowForm);
-
-        })
-
-    })
-
-    function modalOn(modal) {
-        document.body.classList.add('active');
-        overlay.classList.add('active');
-        modal.classList.add('active');
-
-        document.addEventListener('click', (e) => {
-            if (e.target == overlay) {
-                modalOff();
-            }
-        })
-        document.addEventListener('keydown', function(e) {
-            if (e.key === 'Escape') {
-                modalOff();
-            }
+        modalClose.forEach(item => {
+            item.addEventListener('click', modalOff);
         });
 
+        modalBtn.forEach(btn => {
 
-    }
+            btn.addEventListener("click", () => {
+                modalOn(modalWindowForm);
 
-    function modalOff() {
-        document.body.classList.remove('active');
-        overlay.classList.remove('active');
-        modalWindowALL.forEach(modal => {
-            modal.classList.remove('active');
+            })
+
         })
 
+        function modalOn(modal) {
+            document.body.classList.add('active');
+            overlay.classList.add('active');
+            modal.classList.add('active');
 
-
-    }
-
-
-    //form
-
-    const form = document.querySelectorAll('form')
-    const modalWindowTnh = document.querySelector("#modal-tnh")
-
-    form.forEach(form => {
-
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            modalOff();
-            modalOn(modalWindowTnh);
-
-            form.reset();
-
-            setTimeout(() => {
-                if (modalWindowTnh.classList.contains('active')) {
+            document.addEventListener('click', (e) => {
+                if (e.target == overlay) {
                     modalOff();
                 }
+            })
+            document.addEventListener('keydown', function(e) {
+                if (e.key === 'Escape') {
+                    modalOff();
+                }
+            });
 
-            }, 6000)
+
+        }
+
+        function modalOff() {
+            document.body.classList.remove('active');
+            overlay.classList.remove('active');
+            modalWindowALL.forEach(modal => {
+                modal.classList.remove('active');
+            })
+
+
+
+        }
+
+
+        //form
+
+        const form = document.querySelectorAll('form')
+        const modalWindowTnh = document.querySelector("#modal-tnh")
+
+        form.forEach(form => {
+
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+                modalOff();
+                modalOn(modalWindowTnh);
+
+                form.reset();
+
+                setTimeout(() => {
+                    if (modalWindowTnh.classList.contains('active')) {
+                        modalOff();
+                    }
+
+                }, 6000)
 
 
 
 
-            console.log('submit')
+            })
+
+
         })
+    })();
 
 
-    })
 
     // Плавный scroll
 
-    const anchors = document.querySelectorAll('a[href*="#"]');
-    for (let anchor of anchors) {
-        anchor.addEventListener("click", function(e) {
-            e.preventDefault();
-
-            const blockID = anchor.getAttribute("href").substring(1);
-            if (blockID) {
-                document.getElementById(blockID).scrollIntoView({
-                    behavior: "smooth",
-                    block: "start",
-                });
-            } else {
-                return false
-            }
-        });
-    }
+    (function() {
 
 
+        const anchors = document.querySelectorAll('a[href*="#"]');
+        for (let anchor of anchors) {
+            anchor.addEventListener("click", function(e) {
+                e.preventDefault();
+
+                const blockID = anchor.getAttribute("href").substring(1);
+                if (blockID) {
+                    document.getElementById(blockID).scrollIntoView({
+                        behavior: "smooth",
+                        block: "start",
+                    });
+                } else {
+                    return false
+                }
+            });
+        }
 
 
-
-
-
-
+    })();
 
 
 
 
-    // const btn = document.querySelectorAll('[data-accordion="close"]');
-
-    // btn.forEach(btn => {
-
-    //     btn.addEventListener('click', (e) => {
-    //         const btn = e.target;
-    //         const btnAtr = btn.getAttribute('data-accordion')
-    //         const wraper = btn.closest('[data-accordion="wraper"]');
-    //         const content = wraper.querySelector('[data-accordion="content"');
-
-
-    //         console.log
-
-
-
-    //         if (btnAtr == "close") {
-    //             btn.setAttribute('data-accordion', 'open')
-    //             btn.innerText = "Скрыть описание";
-    //             content.style.maxHeight = content.scrollHeight + 'px';
-    //         } else {
-    //             btn.setAttribute('data-accordion', 'close')
-    //             btn.innerText = "Описание ступени";
-    //             content.style.maxHeight = 0;
-    //         }
-
-
-
-
-    //     })
-
-    // });
 
 
 
